62492e62f4b37bddc165005ad46713a507fb7c77
Thomas Leonardo <tl.rp@hotmail.com>
2016-06-02 14:52:08 -0300

    got im

--- server.js
+++ server.js
@@ -5,15 +5,22 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+var fs=require('fs');
 
 var app = express();
 app.get('/api/whoami',function(req,res){
+	fs.writeFile('log.txt',req,function(err){
+		if(err) throw err;
+
+	});
 	var object={};
-	object['ipadress']=req.ip;
-	//object['language']=req
-	console.log(req);
+	var ips=req.ip.split(':');
+	object['ipaddress']=ips[ips.length-1];
+	object['language']=req.headers['user-agent'].split('(')[1].split(')')[0];
+	object['software']=req.headers['accept-language'].split(',')[0];
+	//console.log(req.headers);
 	res.end(JSON.stringify(object));
-})
+});
 
 
 var port = process.env.PORT || 8080;
